df0195878bf5fcba31381b3dcf3ac5bc05196a7d
Abu bakar <106026862+chbakar26@users.noreply.github.com>
2022-09-04 13:54:40 +0500

    Update mod-4.js

--- mod-4.js
+++ mod-4.js
@@ -1,11 +1,11 @@
 (function () {
 	var names = [
 		" hammad",
-		"John sins",
+		" John sins",
 		" mia khilifa",
-		"jerry",
-		"Frank",
-		"Paul",
+		" jerry",
+		" Frank",
+		" Paul",
 	];
 
 	for (var i = 0; i < names.length; i++) {
